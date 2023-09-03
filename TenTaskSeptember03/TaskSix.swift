b1da0cc2e9229efa5f6640c33abffbc4a72c454e
khlestkinaleksey <khlestkinvarshaveraleksey@gmail.com>
2023-09-03 17:06:32 +0300

    solution for task №6

---
+++ TenTaskSeptember03/TaskSix.swift
@@ -0,0 +1,15 @@
+/*
+ 
+ You are given two interior angles (in degrees) of a triangle.
+
+ Write a function to return the 3rd.
+
+ Note: only positive integers will be tested.
+ 
+ */
+
+import Foundation
+
+func otherAngle(a: Int, b: Int) -> Int {
+    180 - a - b
+}
